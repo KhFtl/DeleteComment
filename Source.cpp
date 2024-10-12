64967f01fa76bacf3e2b7ccadda6fcc81c303b02
Виталий Шовкун <vetalshovkun@gmail.com>
2024-10-12 11:26:58 +0300

    Додано функцію, яка читаю файл с програмним кодом.

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,25 @@
 #include<fstream>
 using namespace std;
 
+const string file = "proga.cpp";
+
+string ReadFile(string file)
+{
+	ifstream in(file);
+	string line="", str="";
+	while (!in.eof())
+	{
+		getline(in, line);
+		str += line+'\n';
+	}
+	return str;
+}
+
 int main()
 {
+	string str = ReadFile(file);
+	cout << str << endl;
+	cout << "-----------------------------------------" << endl;
+
 	return 0;
 }
---
+++ proga.cpp
@@ -0,0 +1,38 @@
+//Link library
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+/*const int x_max = 100;
+const int y_max = 25;*/
+
+void GoToXY(int x, int y);
+void CursorVisible(bool visible);
+
+enum Rotation
+{
+    //Value of rotation
+    left,
+    right,
+    up,
+    down
+};
+
+struct Position
+{
+    int x;
+    int y;
+};
+
+class Field
+{
+public:
+    //char field[y_max][x_max + 1];
+    void CreateBoundaries(char ch);
+    void //Show();
+    void SpownPrice();
+};
+
